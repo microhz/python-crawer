902f4c2066808ea2bae9b63bd58329973e5d9986
micro <pengmi2@126.com>
2017-05-12 00:44:30 +0800

    init python

---
+++ test.py
@@ -0,0 +1,23 @@
+#!/bin/python
+# coding=utf-8
+print "hello world"
+
+# python 3.0
+print("Hello python 3.0")
+
+# chinese
+print "中文你好"
+
+# 同行多个语句
+print "语句1";print "语句2"
+
+str = 'a '
+'''
+多行注释
+'''
+print "注释完毕"
+
+#等待用户输入
+# raw_input('wait user to input...');
+
+import sys;n='test';sys.stdout.write(n + "-");
